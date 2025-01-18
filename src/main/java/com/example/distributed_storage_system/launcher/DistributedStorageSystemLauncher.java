2599d61974a0c5a2cc0e50cf9db843f020b2feb7
Sunny <sunny@Sunnys-MacBook-Air.local>
2025-01-18 23:13:49 +0530

    initialised-gradle-project

---
+++ src/main/java/com/example/distributed_storage_system/launcher/DistributedStorageSystemLauncher.java
@@ -0,0 +1,14 @@
+package com.example.distributed_storage_system.launcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedStorageSystemLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedStorageSystemLauncher.class, args);
+		System.out.println("Application started");
+	}
+
+}
